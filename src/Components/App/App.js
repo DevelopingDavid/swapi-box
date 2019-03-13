703b1cc49f89eb6b9c60acdf954e7c0aa8b098fe
David Cisneros <Dcisneros101@gmail.com>
2019-03-13 11:21:58 -0600

    Add textflow and create animation

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,11 +4,27 @@ import TextFlow from '../TextFlow/TextFlow';
 import Deck from '../Deck/Deck';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      crawlText: '',
+      movieTitle: '',
+      releaseDate: '',
+    }
+  }
+
+  componentDidMount() {
+    const randomNum = Math.floor((Math.random() * 7) + 1);
+    fetch(`https://swapi.co/api/films/${randomNum}`)
+    .then(response => response.json())
+    .then(movie => this.setState({crawlText: movie.opening_crawl, movieTitle: movie.title, releaseDate: movie.release_date}))
+  }
+
   render() {
     return (
       <section>
         <Header/>
-        <TextFlow/>
+        <TextFlow movieInfo={this.state}/>
         <Deck/>
       </section>
     );
--- src/Components/TextFlow/TextFlow.js
+++ src/Components/TextFlow/TextFlow.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const TextFlow = () => {
-  return (
-    <section>
 
+const TextFlow = (props) => {
+  return (
+    <section className="textFlow-container">
+      <div className="fade"></div>
+      <div className="star-wars">
+        <div className="crawl">
+          <div className="title">
+            <h3>{props.movieInfo.releaseDate}</h3>
+            <h1>{props.movieInfo.movieTitle}</h1>
+          </div>
+          <p>{props.movieInfo.crawlText}</p>
+        </div>
+      </div>
     </section>
   )
 }
