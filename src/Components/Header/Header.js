a30fa976e3a6910a8e7634fb046e87804b8c6595
David Cisneros <Dcisneros101@gmail.com>
2019-03-17 17:08:45 -0600

    Fix header styles

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,9 +4,7 @@ const Header = (props) => {
   return (
     <section className='header-container'>
       <h1>Swapi-Box</h1>
-      <div className='controls'>
         <Controls retrievePeople={props.retrievePeople} retrievePlanets={props.retrievePlanets} retrieveVehicles={props.retrieveVehicles} setFilter={props.setFilter} state={props.state}/>
-      </div>
     </section>
   )
 }
